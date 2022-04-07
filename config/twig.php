5c66a44b9ae5607ea50aaaed336ac928974e4f7b
HOURDOU DAVID <davidhourdou@gmail.com>
2022-04-07 12:20:00 +0200

    vive les globales

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//globale email àjoutée à l'initialisation de l'objet Twig
+$contactEmail = 'david_hourdou@yahoo.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
